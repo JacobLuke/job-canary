16fd3165e6f5fec9fe93e09fee4b534b889e62b7
JacobLuke <jabraham@jabraham-mn1.linkedin.biz>
2013-07-13 08:26:56 -0700

    added basic registration page

--- job_canary/job_canary/urls.py
+++ job_canary/job_canary/urls.py
@@ -7,15 +7,16 @@ from django.conf.urls.static import static
 import employer.urls
 import jobs.urls
 
-def index(response):
-    return render(response, 'index.html')
+def quick_render(file):
+    return lambda response: render(response, file)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'job_canary.views.home', name='home'),
     # url(r'^job_canary/', include('job_canary.foo.urls')),
-        url(r'^$', index),
-        url(r'^index', index),
+        url(r'^$', quick_render("index.html")),
+        url(r'^index', quick_render("index.html")),
+        url(r'^register', quick_render("register.html")),
 		url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
         url(r'^user/', include(user.urls)),
 		url(r'^admin/', include(admin.site.urls)),

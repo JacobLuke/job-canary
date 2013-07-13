0a8a35b2027d3ec304367690e2574216a0e48564
dcollier <dcollier@uwaterloo.ca>
2013-07-12 20:46:15 -0700

    fixed null stuff, admin

--- job_canary/jobcan/models.py
+++ job_canary/jobcan/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+def content_file_name(instance, filename):
+    return '/'.join(['content', instance.name, filename])
+
+class Company(models.Model):
+  description = models.CharField(max_length=1000)
+  name = models.CharField(max_length=30)
+
 class Cycle(models.Model):
   name = models.CharField(max_length=200)
   description = models.CharField(max_length=1000)
@@ -7,23 +14,19 @@ class Cycle(models.Model):
   end_date = models.DateTimeField('end_date')
   
 class Job(models.Model):
-  cycle = models.ForeignKey(Cycle)
+  cycle = models.ForeignKey(Cycle, blank=True, null=True)
   company = models.ForeignKey(Company)
   title = models.CharField(max_length=300)
   description = models.CharField(max_length=1000)
 
-class Company(models.Model):
-  description = models.CharField(max_length=1000)
-  name = models.CharField(max_length=30)
-
 class Candidate(models.Model):
   description = models.CharField(max_length=1000)
   name = models.CharField(max_length=30)
-  resume  = forms.FileField()
+  resume  = models.FileField(upload_to=content_file_name, blank=True, null=True)
   
 class Application(models.Model):
   description = models.CharField(max_length=1000)
   name = models.CharField(max_length=30)
-  resume  = forms.FileField()
+  resume  = models.FileField(upload_to=content_file_name)
   job = models.ForeignKey(Job)
   candidate = models.ForeignKey(Candidate)

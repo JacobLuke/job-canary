afbe4336e3de0c0e5a01f8e34a220b683a14badb
Jacob Abrahams <jabraham@jabraham-mn1.linkedin.biz>
2013-07-12 21:20:38 -0700

    added some structure, the start of the user view

--- job_canary/jobcan/views/user.py
+++ job_canary/jobcan/views/user.py
@@ -1,3 +1,12 @@
-from models import Person
+
+from djagno.shortcuts import get_object_or_404, render
+from job_canary.models import Candidate
+
 def profile(request):
-  
+    context = {}
+    try:
+        id = request.POST['id']
+        user = get_object_or_404(Candidate, pk=id)
+        context["user"] = user
+    except:pass
+    return render(request, '../../templates/user/profile.html', context)

7baed3f5a292faa11522be83aac7792c816f00c2
JacobLuke <jabraham@jabraham-mn1.linkedin.biz>
2013-07-12 23:49:29 -0700

    adds user's jobs page handling

--- job_canary/job_canary/user/urls.py
+++ job_canary/job_canary/user/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-                       url('^profile', views.user.profile, name="profile")
+                       url('^profile', views.user.profile, name="profile"),
+                       url('^jobs', views.user.jobs, name="Jobs")
                        )
--- job_canary/jobcan/views/user.py
+++ job_canary/jobcan/views/user.py
@@ -1,6 +1,6 @@
 
-from django.shortcuts import get_object_or_404, render
-from jobcan.models import Candidate
+from django.shortcuts import get_object_or_404,get_list_or_404, render
+from jobcan.models import Candidate, Application
 
 def profile(request):
     context = {}
@@ -10,3 +10,15 @@ def profile(request):
         context["user"] = user
     except:raise
     return render(request, 'user/profile.html', context)
+
+def jobs(request):
+    context = {}
+    try:
+        id = request.GET['id']
+        user = get_object_or_404(Candidate, pk=id)
+        context['user'] = user
+        jobs = get_list_or_404(Application, candidate=user)
+        context['jobs'] = jobs
+        print jobs
+    except:raise
+    return render(request, "user/jobs.html", context)

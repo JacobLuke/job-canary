973e7804569cf412b261a26455f67d85834addfe
JacobLuke <jabraham@jabraham-mn1.linkedin.biz>
2013-07-13 00:35:43 -0700

    added a base template for files

--- job_canary/job_canary/user/urls.py
+++ job_canary/job_canary/user/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
                        url('^profile', views.user.profile, name="profile"),
-                       url('^jobs', views.user.jobs, name="Jobs")
+                       url('^jobs', views.user.jobs, name="Jobs"),
+                       url('^upload', views.user.upload, name="Upload file")
                        )
--- job_canary/jobcan/views/user.py
+++ job_canary/jobcan/views/user.py
@@ -2,23 +2,30 @@
 from django.shortcuts import get_object_or_404,get_list_or_404, render
 from jobcan.models import Candidate, Application
 
+def getUser(request):
+    return get_object_or_404(Candidate, pk=request.GET['id'])
+
 def profile(request):
     context = {}
     try:
-        id = request.GET['id']
-        user = get_object_or_404(Candidate, pk=id)
-        context["user"] = user
+        context["user"] = getUser(request)
     except:raise
     return render(request, 'user/profile.html', context)
 
 def jobs(request):
     context = {}
     try:
-        id = request.GET['id']
-        user = get_object_or_404(Candidate, pk=id)
+        user = getUser(request)
         context['user'] = user
         jobs = get_list_or_404(Application, candidate=user)
         context['jobs'] = jobs
         print jobs
     except:raise
     return render(request, "user/jobs.html", context)
+
+def upload(request):
+    context = {}
+    try:
+        context['user'] = getUser(request)
+    except:raise
+    return render(request, 'user/upload.html', context)
